6d2ede22236d98d43af85800463116c5c91a896f
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-31 19:53:45 +0200

    Adding enumeration EHungerGameMessageCode

---
+++ src/main/java/fr/pederobien/minecrafthungergame/EHungerGameMessageCode.java
@@ -0,0 +1,33 @@
+package fr.pederobien.minecrafthungergame;
+
+import fr.pederobien.minecraftdictionary.impl.Permission;
+import fr.pederobien.minecraftdictionary.interfaces.IMinecraftMessageCode;
+
+public enum EHungerGameMessageCode implements IMinecraftMessageCode {
+	PLAYER_DONT_REVIVE(Permission.ALL);
+
+	private Permission permission;
+
+	private EHungerGameMessageCode() {
+		this(Permission.OPERATORS);
+	}
+
+	private EHungerGameMessageCode(Permission permission) {
+		this.permission = permission;
+	}
+
+	@Override
+	public String value() {
+		return toString();
+	}
+
+	@Override
+	public Permission getPermission() {
+		return permission;
+	}
+
+	@Override
+	public void setPermission(Permission permission) {
+		this.permission = permission;
+	}
+}
