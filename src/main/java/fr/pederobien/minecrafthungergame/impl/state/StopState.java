f47e37760c9064cf41741d9bc184909502defd4f
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-06-02 18:26:41 +0200

    Implementing StopState

--- src/main/java/fr/pederobien/minecrafthungergame/impl/state/StopState.java
+++ src/main/java/fr/pederobien/minecrafthungergame/impl/state/StopState.java
@@ -1,10 +1,27 @@
 package fr.pederobien.minecrafthungergame.impl.state;
 
+import org.bukkit.GameMode;
+
+import fr.pederobien.minecraftgameplateform.helpers.TeamHelper;
+import fr.pederobien.minecraftgameplateform.utils.Plateform;
 import fr.pederobien.minecrafthungergame.interfaces.IHungerGame;
+import fr.pederobien.minecraftmanagers.PlayerManager;
+import fr.pederobien.minecraftmanagers.WorldManager;
 
 public class StopState extends AbstractState {
 
 	public StopState(IHungerGame game) {
 		super(game);
+	}
+
+	@Override
+	public void stop() {
+		WorldManager.OVERWORLD.getWorldBorder().reset();
+		WorldManager.NETHER_WORLD.getWorldBorder().reset();
+		WorldManager.END_WORLD.getWorldBorder().reset();
+		TeamHelper.removeTeamsFromServer(getConfiguration().getTeams());
+		PlayerManager.setGameModeOfAllPlayers(GameMode.CREATIVE);
+		Plateform.getTimeTask().stop();
+		getGame().setCurrentState(getGame().getInitialState());
 	}
 }
