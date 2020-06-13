074f37cf962f9ba057a7e33877b0bf4067e68869
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-06-13 20:30:37 +0200

    Adding enumeration EHungerGameLabel

---
+++ src/main/java/fr/pederobien/minecrafthungergame/commands/EHungerGameLabel.java
@@ -0,0 +1,18 @@
+package fr.pederobien.minecrafthungergame.commands;
+
+import fr.pederobien.minecraftgameplateform.interfaces.element.ILabel;
+
+public enum EHungerGameLabel implements ILabel {
+	PLAYER_DONT_REVIVE_TIME("playerDontReviveTime");
+
+	private String label;
+
+	private EHungerGameLabel(String label) {
+		this.label = label;
+	}
+
+	@Override
+	public String getLabel() {
+		return label;
+	}
+}
