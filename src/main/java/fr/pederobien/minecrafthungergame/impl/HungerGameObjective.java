a64d98a1bd2a6094ef1e09a950a45f22a6864ede
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-06-15 14:16:51 +0200

    Upgrading HungerGameObjective

--- src/main/java/fr/pederobien/minecrafthungergame/impl/HungerGameObjective.java
+++ src/main/java/fr/pederobien/minecrafthungergame/impl/HungerGameObjective.java
@@ -8,13 +8,10 @@ import org.bukkit.scoreboard.DisplaySlot;
 
 import fr.pederobien.minecraftgameplateform.entries.auto.LocationAutoUpdater;
 import fr.pederobien.minecraftgameplateform.entries.auto.OrientationAutoUpdater;
-import fr.pederobien.minecraftgameplateform.entries.auto.TimeTaskAutoUpdater;
-import fr.pederobien.minecraftgameplateform.entries.auto.WorldBorderSizeUpdater;
+import fr.pederobien.minecraftgameplateform.entries.auto.WorldBorderSizeCountDownAutoUpdater;
 import fr.pederobien.minecraftgameplateform.entries.simple.CenterEntry;
-import fr.pederobien.minecraftgameplateform.entries.simple.GameTimeEntry;
 import fr.pederobien.minecraftgameplateform.entries.simple.LocationEntry;
-import fr.pederobien.minecraftgameplateform.entries.simple.PauseTimeEntry;
-import fr.pederobien.minecraftgameplateform.entries.simple.TotalTimeEntry;
+import fr.pederobien.minecraftgameplateform.entries.simple.WorldBorderSizeCountDownEntry;
 import fr.pederobien.minecraftgameplateform.impl.element.GameObjective;
 import fr.pederobien.minecraftgameplateform.interfaces.element.IBorderConfiguration;
 import fr.pederobien.minecrafthungergame.interfaces.IHungerGameConfiguration;
@@ -82,11 +79,7 @@ public class HungerGameObjective extends GameObjective<IHungerGameConfiguration>
 		add(score -> new OrientationAutoUpdater(this, new CenterEntry(score, getConfiguration().getBorder(WorldManager.OVERWORLD).get().getBorderCenter())));
 		emptyEntry(score--);
 		for (IBorderConfiguration border : getConfiguration().getBorders())
-			add(score -> new WorldBorderSizeUpdater(this, score, border, "#", true));
-		emptyEntry(score--);
-		add(score -> new TimeTaskAutoUpdater(this, new TotalTimeEntry(score)));
-		add(score -> new TimeTaskAutoUpdater(this, new PauseTimeEntry(score)));
-		add(score -> new TimeTaskAutoUpdater(this, new GameTimeEntry(score)));
+			add(score -> new WorldBorderSizeCountDownAutoUpdater(this, new WorldBorderSizeCountDownEntry(score, border, "#").setDisplayHalfSize(true)));
 	}
 
 	private void add(Function<Integer, IEntry> constructor) {
