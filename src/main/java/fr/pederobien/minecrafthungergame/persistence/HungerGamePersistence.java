94b7865ec1d2ad09912200c1e1f41b10b558725e
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-06-15 15:04:13 +0200

    Updating project du to update in project fr.pederobien.minecraftgameplateform

--- src/main/java/fr/pederobien/minecrafthungergame/persistence/HungerGamePersistence.java
+++ src/main/java/fr/pederobien/minecrafthungergame/persistence/HungerGamePersistence.java
@@ -3,13 +3,14 @@ package fr.pederobien.minecrafthungergame.persistence;
 import fr.pederobien.minecraftgameplateform.impl.element.persistence.AbstractMinecraftPersistence;
 import fr.pederobien.minecraftgameplateform.interfaces.element.persistence.IMinecraftPersistence;
 import fr.pederobien.minecraftgameplateform.utils.Plateform;
+import fr.pederobien.minecrafthungergame.HungerGameConfiguration;
 import fr.pederobien.minecrafthungergame.interfaces.IHungerGameConfiguration;
 import fr.pederobien.minecrafthungergame.persistence.loaders.HungerGameLoaderV10;
 
 public class HungerGamePersistence extends AbstractMinecraftPersistence<IHungerGameConfiguration> {
 
 	private HungerGamePersistence() {
-		super(Plateform.ROOT.resolve("HungerGame"), new DefaultHungerGameContent());
+		super(Plateform.ROOT.resolve("HungerGame"), "DefaultHungerGameConfiguration");
 		register(new HungerGameLoaderV10());
 	}
 
@@ -19,6 +20,12 @@ public class HungerGamePersistence extends AbstractMinecraftPersistence<IHungerG
 
 	private static class SingletonHolder {
 		public static final IMinecraftPersistence<IHungerGameConfiguration> PERSISTENCE = new HungerGamePersistence();
+	}
+
+	@Override
+	public void saveDefault() {
+		set(new HungerGameConfiguration(getDefault()));
+		save();
 	}
 
 	@Override
