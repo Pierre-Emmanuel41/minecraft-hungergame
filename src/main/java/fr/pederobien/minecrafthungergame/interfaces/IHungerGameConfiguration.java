dc4e2d07ac66d51b961b7fae7eeb38bb27767830
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2020-07-10 16:40:38 +0200

    Deleting class IUnmodifiableHungerGameConfiguration

--- src/main/java/fr/pederobien/minecrafthungergame/interfaces/IHungerGameConfiguration.java
+++ src/main/java/fr/pederobien/minecrafthungergame/interfaces/IHungerGameConfiguration.java
@@ -4,7 +4,15 @@ import java.time.LocalTime;
 
 import fr.pederobien.minecraftgameplateform.interfaces.element.IGameBorderConfiguration;
 
-public interface IHungerGameConfiguration extends IUnmodifiableHungerGameConfiguration, IGameBorderConfiguration {
+public interface IHungerGameConfiguration extends IGameBorderConfiguration {
+
+	/**
+	 * While the game time is less than this time, if a player dies, it will respawn in game mode survival and keep its inventory.
+	 * Once this time is exceed, a player will respawn in spectator mode and drop its inventory if it dies.
+	 * 
+	 * @return The time after which a player respawn in spectator mode.
+	 */
+	LocalTime getPlayerDontReviveTime();
 
 	/**
 	 * While the game time is less than this time, if a player dies, it will respawn in game mode survival and keep its inventory.
